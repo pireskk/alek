80fb8dcd7daf8d1fb0e7ecafe3fb30702ef12af7
pireskk <kalilviturino4@gmail.com>
2021-03-13 17:15:04 -0400

    First commit

---
+++ src/zeusmenu.js
@@ -0,0 +1,39 @@
+const zeusmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do PIRES:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR *PIRES*
+  grupo:https://chat.whatsapp.com/CWDE10hFK31JwBN79IWJLB
+╚════════════════════`
+}
+
+exports.zeusmenu = zeusmenu
+
+
+
+
+
+
+
+
